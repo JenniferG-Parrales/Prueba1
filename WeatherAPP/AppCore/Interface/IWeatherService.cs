e1ac15c512f3452caf26714fdb07cbd85f6263c5
Je'nn <85895809+JenniferG-Parrales@users.noreply.github.com>
2022-04-03 22:23:28 -0600

    Add files via upload

---
+++ WeatherAPP/AppCore/Interface/IWeatherService.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities.Weather;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCore.Interface
+{
+    public interface IWeatherService
+    {
+        Root GetWeather(string Ciudad);
+    }
+}
