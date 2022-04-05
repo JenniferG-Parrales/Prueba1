d3f43b309e88b55df34d7fb732a3cdc6e90036cf
Je'nn <85895809+JenniferG-Parrales@users.noreply.github.com>
2022-04-05 17:03:25 -0600

    Add files via upload

---
+++ Domain/Interface/IModel.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities.Weather;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interface
+{
+    public interface IModel
+    {
+        Root GetWeather(string Ciudad);
+    }
+}
