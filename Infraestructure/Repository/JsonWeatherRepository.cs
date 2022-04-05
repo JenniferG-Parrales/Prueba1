31e107b9a86097a9d4b9c6d1ecd50265de9d807e
Je'nn <85895809+JenniferG-Parrales@users.noreply.github.com>
2022-04-05 11:53:47 -0600

    Add files via upload

---
+++ Infraestructure/Repository/JsonWeatherRepository.cs
@@ -0,0 +1,40 @@
+﻿using Domain.Entities.Weather;
+using Domain.Interface;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure
+{
+    public class JsonWeatherRepository : IModel
+    {
+        public Root Info;
+        string APIKey = "abefba56a9c6af23f7372440f4d1d63b";
+
+        public Root GetWeather(string Ciudad)
+        {
+            using (WebClient web = new WebClient())
+            {
+                try
+                {
+                    string url = string.Format("https://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}",Ciudad,APIKey);
+                var json = web.DownloadString(url);
+                Info = JsonConvert.DeserializeObject<Root>(json);
+                
+
+                return Info;
+                }
+
+                catch (Exception)
+                {
+                    return Info = null;
+                }
+            }
+        }
+    }
+}
