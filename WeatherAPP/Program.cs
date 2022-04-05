09c5a05d34d4eed7308e1f2b0d861f821aca33f9
Je'nn <85895809+JenniferG-Parrales@users.noreply.github.com>
2022-04-05 17:42:22 -0600

    Add files via upload

---
+++ WeatherAPP/Program.cs
@@ -0,0 +1,32 @@
+﻿using AppCore.Interface;
+using AppCore.Services;
+using Autofac;
+using Domain.Interface;
+using Infraestructure;
+using Infraestructure.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeatherAPP
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            var builder = new ContainerBuilder();
+            builder.RegisterType<JsonWeatherRepository>().As<IModel>();
+            builder.RegisterType<WatherService>().As<IWeatherService>();
+            var container = builder.Build();
+            Application.Run(new Form1(container.Resolve<IWeatherService>()));
+        }
+    }
+}
